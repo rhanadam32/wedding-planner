77315a7d36904b7e8deb3832136665a7bbbbc568
Raihan Adam <raihanadam321@gmail.com>
2026-08-18 23:13:25 +0700

    feat: initialize Vue 3 project structure with routing, API service, and base login view

---
+++ frontend/src/view/auth/login.vue
@@ -0,0 +1,16 @@
+<script setup lang='ts'>
+
+</script>
+
+<template>
+    <div class="container">
+        <div class="card">
+            <div class="card-header">
+                Login
+            </div>
+            <div class="card-body">
+                <h4 class="card-title">Login Wedding Planner</h4>
+            </div>
+        </div>
+    </div>
+</template>
